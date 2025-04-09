84568aefcb8c87f5d0ec95216f3e5eb3ce5468e0
grabstacker <bradkay12@gmail.com>
2025-04-09 13:37:12 +1200

    added CLI data entry book search

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
+import sys
 #import stats functions from other file stats.py in same directory.
 from stats import count_words, num_char
 
 #present the output in a nice readable format and list the dic values decending based on key count.
 def main():
-    book_path = "books/frankenstein.txt"
-    text = get_book_text(book_path)
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book = sys.argv[1]
+    #book_path = input("Please enter a book is this format \n(directory/bookname.txt): books/frankenstein.txt: \n")
+    text = get_book_text(f"{book}")
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {book_path}...")
+    print(f"Analyzing book found at {book}...")
     print("----------- Word Count ----------")
     print(f"Found {count_words(text)} total words")
     print("--------- Character Count -------")    
